466303139a8b7f5daeac57f9d39892a894e149b2
DifferF <differ.free@gmail.com>
2022-10-25 18:11:18 +0300

    Initial commit

---
+++ src/main/java/homework_05/HibernateUtil.java
@@ -0,0 +1,29 @@
+package homework_05;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by Asus on 01.11.2017.
+ */
+public class HibernateUtil {
+
+    private static SessionFactory factory;
+
+
+    static {
+        try {
+             factory = new Configuration()
+                    .configure("ex_002_config.xml")
+                    .buildSessionFactory();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+
+}
